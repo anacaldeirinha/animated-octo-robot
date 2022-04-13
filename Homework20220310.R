7cf616895c534454347e40b583c6d83e18a57b79
anacaldeirinha <ana.caldeirinha13242@hotmail.com>
2022-04-13 16:17:33 +0300

    b

--- Homework20220310.R
+++ Homework20220310.R
@@ -18,30 +18,26 @@ for (i in 1:10){
 
 
 #####Problem 3#####
-# What is the probability that out of a group of 6 men and 8 women, if we pick
-# 5 people at random, exactly 3 will be men?
-# Use a for loop, which simulates the picking.
-
-
-mw<-c(1,1,1,1,1,1,
-      0,0,0,0,0,0,0,0)
-pick<-NULL
-result<-NULL
-
-for (i in 1:5){
-  pick<-sample(1:14,1)
-  if (pick > 6){
-    result=c(result,0)
-  } else {
-    result=c(result,1)
-  }
-}
 
 ResultS<-sum(result)
 
 All<-choose(14,5)
 
-probability<-(ResultS/All)*100
-P<-c*100
+
+people<-c(rep("men",6),rep("women",8))
+resultvector<-NULL
+
+for(i in 1:1000){ 
+  choose<-sample(people,size=4,replace=FALSE)
+  if(sum(choose=="men")==3){
+    resultvector<-c(resultvector,1)
+  } else{
+    resultvector<-c(resultvector,0)
+  }
+}
+
+Sum<-sum(resultvector)
+
+probability<-(Sum/1000)*100
 
 #####Problem 4#####
