6227cdafe64689bb74b5783f47a02b4d77a0f991
anacaldeirinha <ana.caldeirinha13242@hotmail.com>
2022-04-24 15:53:27 +0300

    b

--- Homework20220407.R
+++ Homework20220407.R
@@ -1,18 +1,14 @@
 #####Problem 1#####
 
-# 5.Select the last observation for each stock, for each month. 
-# In order to do this, first create a new column, which will show you the 
-# year and the month. You can do this using the functions substr() or floor_date.
 
 library(tidyquant)
 library(dplyr)
 library(tidyverse)
+library(lubridate)
 
+#1 and 2
 
-Dates<-data.frame(Dates=seq.Date(from = ymd("2019-01-01"),
-                                 to = ymd("2021-04-01"),by = "day"))
-
-
+#First tought
 stockpricesF<-tq_get("FB")
 
 FinalF<-Dates%>%
@@ -41,7 +37,29 @@ FinalN<-Dates%>%
   fill(adjusted)%>%
   select(1:2,adjusted)
 
-  
+
+#Learn in class
+
+
+data<- tq_get(c("AMZ","FB","NFLX"),
+              from = "2019-01-01",
+              to = "2021-04-01")%>%
+  select (symbol, date, adjusted)
+
+
+
+Dates <- data.frame(Date = rep(seq.Date(from = ymd("2019-01-01"),
+                                      to = ymd ("2021-04-01"),
+                                      by = "day"), 3),
+                  Symbol = c(rep("AMZ",822), rep ("FB", 822), rep ("NFLX",822)))
+
+Join <- Dates %>%
+  dplyr:: left_join(data, by = c("Date" = "date", "Symbol"="symbol"))%>%
+  group_by(Symbol)%>%
+  fill(adjusted, .direction = "downup")
+ 
+
+#3 and 4
 
 Q2<-tq_get(c("FB","AMZN"))%>%
   filter(date>=ymd("2019-01-01"),
@@ -54,10 +72,17 @@ Q2<-tq_get(c("FB","AMZN"))%>%
            row_number() == sum(symbol == "AMZN")|
            row_number() == (sum(symbol == "AMZN")+1))
 
+#5
+Q5<-tq_get(c("FB","AMZN"))%>%
+  filter(date>=ymd("2019-01-01"),
+         date<=ymd("2019-07-01")|
+           date>=ymd("2020-04-01"),
+         date<=ymd("2020-07-01"))%>%
+  arrange(symbol)%>%
+  mutate(m = floor_date(date ,unit = c("month")))%>%
+  
 
-
-
-#####Problem 2#####
+######Problem 2#####
 #Use the dataframe from problem 1.2.
 # Use the SMA function from the tidyquant package to calculate the 10day SMA 
 # and the 26 day SMA for each of the 3 stocks. 
